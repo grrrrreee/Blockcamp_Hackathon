09acbb9793b6b1ea9413f8b7a6b206ac41fe4cdb
oeritas <advanwith@gmail.com>
2018-10-28 01:51:21 +0900

    create page:ing

--- e-voting/src/Pages/Board.js
+++ e-voting/src/Pages/Board.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import Create from './Create'
 
 class Board extends Component {
     constructor(props) {
         super(props);
+
+        this._toCreate = this._toCreate.bind(this);
         
         this.state = {
+            clicked : false
         };
     }
 
     componentDidMount(){
-        
+        let pk = this.props.publicKey;
+        let _depart = this.props.depart;
+
+        axios({
+            method: 'post',
+            url: '/user/12345',
+            data: {
+              publicKey: pk,
+              depart: _depart
+            }
+          }).then((res) => {
+            console.log(res);
+        });
+    }
+
+    _toCreate(){
+        this.setState({
+            clicked : true
+        })
     }
 
     render() {
-        return(
-            <div>
-                <p>Hello It's Board</p> 
-                {this.props.publicKey}
-            </div>
-        )
+        if(this.state.clicked === false){
+            return(
+                <div>
+                    <p>Hello It's Board</p> 
+                    {this.props.publicKey}
+                    <p>This is your Voting List</p>
+                    <div>
+                        List
+                    </div>
+                    <p><input type="button" value="Create Vote" onClick={this._toCreate}/></p>
+                </div>
+            )
+        } else if(this.state.clicked === true) {
+            return(
+                <Create />
+            )
+        }
+        
     }
 }
 
