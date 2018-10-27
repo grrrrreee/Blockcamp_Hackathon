51c43ce2de76273ac2c7113e24ab8dd4bc7ca2e3
oeritas <advanwith@gmail.com>
2018-10-27 11:57:54 +0900

    remove Index class + auth:ing

--- e-voting/src/App.js
+++ e-voting/src/App.js
@@ -5,48 +5,52 @@ import './App.css';
 class CertID extends Component {
   constructor(props) {
     super(props);
+    //this._isAuthed = this._isAuthed.bind(this);
+    this.state = {
+      name = "",
+      number = "",
+      depart = "",
+    }
+  }
+
+  _inputName(event){
+    let _name = event.target.value;
+    this.setState({
+      name = _name
+    })
+  }
+
+  _inputNumber(event){
+    let _number = event.target.value;
+    this.setState({
+      number = _number
+    })
+  }
+
+  _inputDepart(event){
+    let _depart = event.target.value;
+    this.setState({
+      depart = _depart
+    })
   }
 
   render() {
     return(
       <div>
-        이름 : <input type="text"/>
-        학번 : <input type="text"/>
-        단과대학 : <input type="text"/>
-        <input type="button"/>
+        이름 : <input type="text" onChange={this._inputName()}/>
+        학번 : <input type="text" onChange={this._inputNumber()}/>
+        단과대학 : <input type="text" onChange={this._inputDepart()}/>
+        <input type="button" onClick={this.props._isAuthed(this.state.name, this.state.number, this.state.depart)}> Submit </input>
       </div>
     )
   }
-
 }
 
 class Index extends Component {
   constructor(props) {
     super(props);
   }
-
-  /*
-  handleMouseMove(event) {
-  }
-  */
-  
-    render() {
-      if(this.props.auth == false){
-        return(
-          <div>
-            <CertID />
-          </div>
-        )
-      } else {
-        return(
-          <div>
-            auth-ed 이후 화면
-          </div>
-        )
-      }
-    }
 }
-//{this.props.render(this.state)}
 
 class App extends Component {
   constructor(props) {
@@ -54,7 +58,6 @@ class App extends Component {
     this._isAuthed = this._isAuthed.bind(this);
     this.state = {
       auth : false,
-
     };
   }
 
@@ -62,8 +65,14 @@ class App extends Component {
     //data fetch
   }
   
-  _isAuthed() {
+  _isAuthed(name, number, depart) {
     let isCert = false;
+
+    if(name!="" && number!="" && depart!=""){
+      isCert = true;
+    } else {
+      alert("hey missed any part!");
+    }
 
     if(isCert == true) {
       this.setState({
@@ -72,24 +81,44 @@ class App extends Component {
     } else {
       this.setState({
         auth: false
-      })
+      });
     }
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            C-Link
-          </p>
-        </header>
-        <body>
-          <Index name="C-Link" auth={this.state.auth} />
-        </body>
-      </div>
-    );
+    if(this.state.auth == false){
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              C-Link
+            </p>
+          </header>
+          <body>
+            <CertID _isAuthed={this._isAuthed()}/>
+          </body>
+        </div>
+      );
+    } else {
+
+    }
+  }
+
+  render() {
+    if(this.props.auth == false) {
+      return(
+        <div>
+          <CertID _isAuthed={this.props._isAuthed()}/>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          auth-ed 이후 화면
+        </div>
+      )
+    }
   }
 }
 
