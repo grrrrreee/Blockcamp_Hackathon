716533784df29fe75e4074f9ef2262cb70783162
oeritas <advanwith@gmail.com>
2018-10-27 13:52:27 +0900

    certify ok

--- e-voting/src/App.js
+++ e-voting/src/App.js
@@ -2,61 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class CertID extends Component {
-  constructor(props) {
-    super(props);
-    //this._isAuthed = this._isAuthed.bind(this);
-    this.state = {
-      name = "",
-      number = "",
-      depart = "",
-    }
-  }
-
-  _inputName(event){
-    let _name = event.target.value;
-    this.setState({
-      name = _name
-    })
-  }
-
-  _inputNumber(event){
-    let _number = event.target.value;
-    this.setState({
-      number = _number
-    })
-  }
-
-  _inputDepart(event){
-    let _depart = event.target.value;
-    this.setState({
-      depart = _depart
-    })
-  }
-
-  render() {
-    return(
-      <div>
-        이름 : <input type="text" onChange={this._inputName()}/>
-        학번 : <input type="text" onChange={this._inputNumber()}/>
-        단과대학 : <input type="text" onChange={this._inputDepart()}/>
-        <input type="button" onClick={this.props._isAuthed(this.state.name, this.state.number, this.state.depart)}> Submit </input>
-      </div>
-    )
-  }
-}
-
-class Index extends Component {
-  constructor(props) {
-    super(props);
-  }
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this._handleSubmit = this._handleSubmit.bind(this);
     this._isAuthed = this._isAuthed.bind(this);
+    this._inputName = this._inputName.bind(this);
+    this._inputNumber = this._inputNumber.bind(this);
+    this._inputDepart = this._inputDepart.bind(this);
+
     this.state = {
+      name : "",
+      number : "",
+      depart : "",
       auth : false,
     };
   }
@@ -64,29 +23,52 @@ class App extends Component {
   componentDidMount() {
     //data fetch
   }
+
+  _inputName(e){
+    let _name = e.target.value;
+    this.setState({
+      name : _name
+    })
+  }
+
+  _inputNumber(e){
+    let _number = e.target.value;
+    this.setState({
+      number : _number
+    })
+  }
+
+  _inputDepart(e){
+    let _depart = e.target.value;
+    this.setState({
+      depart : _depart
+    })
+  }
+
+  _handleSubmit(){
+    this._isAuthed(this.state.name, this.state.number, this.state.depart);
+  }
   
   _isAuthed(name, number, depart) {
     let isCert = false;
 
-    if(name!="" && number!="" && depart!=""){
+    if(name!=="" && number!=="" && depart!==""){
       isCert = true;
-    } else {
+    } else if(name==="" || number==="") {
       alert("hey missed any part!");
     }
 
-    if(isCert == true) {
+    if(isCert === true) {
       this.setState({
         auth: true
       });
     } else {
-      this.setState({
-        auth: false
-      });
+      console.log("false");
     }
   }
 
   render() {
-    if(this.state.auth == false){
+    if(this.state.auth === false){
       return (
         <div className="App">
           <header className="App-header">
@@ -95,27 +77,26 @@ class App extends Component {
               C-Link
             </p>
           </header>
-          <body>
-            <CertID _isAuthed={this._isAuthed()}/>
-          </body>
+          <div>
+            이름 : <input type="text" onChange={(e)=> {this._inputName(e)}}/>
+            학번 : <input type="text" onChange={(e) => {this._inputNumber(e)}}/>
+            단과대학 : <input type="text" onChange={(e)=>{this._inputDepart(e)}}/>
+            <input type="button" value="Submit" onClick={this._handleSubmit} />
+          </div>
         </div>
       );
     } else {
-
-    }
-  }
-
-  render() {
-    if(this.props.auth == false) {
-      return(
-        <div>
-          <CertID _isAuthed={this.props._isAuthed()}/>
-        </div>
-      )
-    } else {
-      return(
-        <div>
-          auth-ed 이후 화면
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              C-Link
+            </p>
+          </header>
+          <div>
+            You are certified!
+          </div>
         </div>
       )
     }
