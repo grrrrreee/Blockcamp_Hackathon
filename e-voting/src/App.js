a4fd087d44a79f8320487e3a633ef00c509844fd
oeritas <advanwith@gmail.com>
2018-10-27 11:33:44 +0900

    branch test + nested component start

--- e-voting/src/App.js
+++ e-voting/src/App.js
@@ -2,50 +2,80 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class CertID extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        이름 : <input type="text"/>
+        학번 : <input type="text"/>
+        단과대학 : <input type="text"/>
+        <input type="button"/>
+      </div>
+    )
+  }
+
+}
+
 class Index extends Component {
   constructor(props) {
     super(props);
-
   }
 
   /*
   handleMouseMove(event) {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    });
   }
   */
-
-  /*
-    Instead of providing a static representation of what <Mouse> renders,
-    use the `render` prop to dynamically determine what to render.
-  */
-
-  render(){
-    return(
-      <div>
-        초기화면 컴포넌트 구성중
-        ?? == {this.props.name}
-        
-      </div>
-    )
-  }
+  
+    render() {
+      if(this.props.auth == false){
+        return(
+          <div>
+            <CertID />
+          </div>
+        )
+      } else {
+        return(
+          <div>
+            auth-ed 이후 화면
+          </div>
+        )
+      }
+    }
 }
-
 //{this.props.render(this.state)}
 
 class App extends Component {
   constructor(props) {
     super(props);
-    //this.handleMouseMove = this.handleMouseMove.bind(this);
-    //this.state = { x: 0, y: 0 };
+    this._isAuthed = this._isAuthed.bind(this);
+    this.state = {
+      auth : false,
+
+    };
   }
 
-  //life cycle
-  //data fetch
-  //function to network with back
+  componentDidMount() {
+    //data fetch
+  }
   
+  _isAuthed() {
+    let isCert = false;
+
+    if(isCert == true) {
+      this.setState({
+        auth: true
+      });
+    } else {
+      this.setState({
+        auth: false
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -56,7 +86,7 @@ class App extends Component {
           </p>
         </header>
         <body>
-          <Index name="C-Link"/>
+          <Index name="C-Link" auth={this.state.auth} />
         </body>
       </div>
     );
@@ -64,9 +94,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-<Index render={mouse => (
-  <Cat mouse={mouse} />
-)}/>
-*/
